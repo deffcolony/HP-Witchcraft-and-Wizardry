e4bb9beb41f57a7ff59b84fa9bb8a0e64742d686
CombatWwombat <jdve212@gmail.com>
2022-04-27 21:19:30 +0100

    started coding for bus arriving

--- resources/scripts/magnolia crescent.lua
+++ resources/scripts/magnolia crescent.lua
@@ -3,20 +3,38 @@ function init()
 end
 
 function tick(dt)
-	timer = timer - dt	
-	
-	light = FindLight("light", true)
-	
-	lamp = FindShape("lamp", true)
+	playerpos = GetPlayerPos()
 
-	if timer <= 1 then
-		table = {0, 10, 30}
-		rand = table[math.random(1, #table)]
-		SetLightIntensity(light, rand)
-		SetShapeEmissiveScale(lamp, rand)
-	end
+	trig = FindTrigger("trig", true)
+	
+	if IsPointInTrigger(trig, playerpos) then
+		timer = timer - dt	
+	
+		light = FindLight("light", true)
+	
+		lamp = FindShape("lamp", true)
 
-	if timer <= 0 then
-		timer = timer + 1.5
+		if timer <= 1 then
+			table = {0, 10, 30}
+			rand = table[math.random(1, #table)]
+			SetLightIntensity(light, rand)
+			SetShapeEmissiveScale(lamp, rand)
+		end
+
+		if timer <= 0 then
+			timer = timer + 1.5
+		end
+		
+		round = FindJoint("round", true)
+		seesaw = FindJoint("seesaw", true)
+		SetJointMotor(round, 0.1)
+		SetJointMotor(seesaw, 0.2)
+		
+		swing = FindBody("swing", true)
+		SetBodyAngularVelocity(swing, 0.3)
+		
+	else
+		SetJointMotor(round,0)
+		SetJointMotor(seesaw,0)
 	end
 end
