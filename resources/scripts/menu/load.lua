277814c7ca76e22ee04805d71f26e7169e5f32bb
Alex <alex200.veebee@gmail.com>
2024-08-14 15:53:58 +0000

    bruh

--- resources/scripts/menu/load.lua
+++ resources/scripts/menu/load.lua
@@ -1,5 +1,5 @@
 menu_levels = {"MOD/privetdrive.xml"}
 
 function tick()
-	StartLevel("Harry Potter", menu_levels[math.random(#menu_levels)])
+	StartLevel("Harry Potter", menu_levels[math.random(#menu_levels)], "menu")
 end
